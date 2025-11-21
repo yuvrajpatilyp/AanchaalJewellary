789677284ee8caf6343006503ddb416c76f36319
yuvrajpatilyp <yuvraj_sp.starsoftech@outlook.com>
2025-11-21 12:07:06 +0530

    Initialized the files

---
+++ src/main/java/com/example/AnchalJewellery/AnchalJewelleryApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnchalJewellery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchalJewelleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnchalJewelleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AnchalJewellery/AnchalJewelleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnchalJewellery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchalJewelleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
