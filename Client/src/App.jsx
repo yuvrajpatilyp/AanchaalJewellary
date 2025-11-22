12efa3286d932d733b20c50707d969758ab4d808
AkifPanariAp <Akif_ap.starsoftech@outlook.com>
2025-11-22 14:22:57 +0530

    created a branch

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-amber-950 flex px-4 py-4 space-x-20'>
+        <title>Aanchal Jewllary</title>
+        <h1 className=' font-serif text-white'>Download The Tanishq App Now </h1>
+        <h1 className='font-serif text-white'>Useful Links</h1>
+        <h1 className='font-serif'></h1>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
